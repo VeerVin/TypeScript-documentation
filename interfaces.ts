de0e4eb978c0695940ea61a9af249eed9a892dc2
Veer Pratap Singh <VeerPrathap.Singh@cognizant.com>
2019-04-17 16:50:21 +0530

    TS: Simplifying understanding on intterfaces

--- interfaces.ts
+++ interfaces.ts
@@ -3,7 +3,86 @@
 /** 
  * An interface is a syntactical contract that an entity should conform to. In other words, 
  * an interface defines the syntax that any entity must adhere to.
+ * 
+ * By defining an interface we are defining the shape of the object, the type of value each property
+ * will have. We can say it is ustom type, which is created by you :)
+ * 
+ * So that we dont pass a number type instead of an string type.
 */
+
+// For an example:
+
+function drawPoint(point){
+    point.x;
+    point.y;
+}
+
+drawPoint({
+    x: 10,
+    y: 20
+});
+
+/**
+ * In the above example we are passing an object which is having required properties to draw
+ * a point. But what if we pass string to 'x' and 'y' properties? our draPoint function code will break. 
+ * Becuase it wants integer type to draw the point not string type. So what is the solution?
+ * 
+ * There are two ways to solve this problem one way is to use inline annotations and other is using Interfaces
+ */
+
+// Inline annotations
+
+function drawPoints(point: {x: number, y: number}){
+    point.x;
+    point.y;
+}
+
+drawPoint({
+    x: 10,
+    y: 20
+});
+
+/**
+ * So in the above example we are adding type annotations to the parameter telling which type of value the 
+ * object parameter should contain. This is called inline annotations.
+ * 
+ * It looks fine for simple cases, but the problem with this is it is verbose and what if we have lot of
+ * properties in our object defining its type in parameter again and again is an headache.
+ * 
+ * What if another function is accepting the same pramters passed a object with same type? we
+ * have to write it again.
+ * 
+ * To solve this problem we can use Interfaces in TypeScript
+ */
+
+// Using Interface
+
+interface Points {
+    x: number;
+    y: number;
+}
+
+function drawPointss(point: Points){
+    point.x;
+    point.y;
+}
+
+drawPointss({
+    x: 30,
+    y: 50
+});
+
+/**
+ * So in the above interface we are defining the shape and type fo the value the object properties
+ * will store and then we are passing it to function parameters to tell the type of value the
+ * parameter will be holding.
+ * 
+ * In this code 'point: Points' interface is informaing the function what type of value the paramter
+ * holds.
+ * 
+ * Note: While naming interface allways use pascal naming convection, by that I mean the 
+ * starting letter of name should be cpaital.
+ */
 
 // Simple Example:
 
@@ -37,7 +116,9 @@ function printLabels(labelObj: printObjVal){
     console.log(labelObj.size); // print: 10
 }
 
-printLabels(printObj); // printObj is decalred in line no: 14, which is passed here
+let printObjs = {size: 10, label: 'Hello!'};
+
+printLabels(printObjs); // printObj is decalred in line no: 14, which is passed here
 
 /**
  * The only change between this example using 'interface' and the above example is, the interface
@@ -79,7 +160,7 @@ function createSquare(config: SquareConfig): {color: string; area: number} {
 let mySquare = createSquare({ color: "black" });
 
 /**
- * By Adding '?' after the interface property name, the property name becomes optionla by that 
+ * By Adding '?' after the interface property name, the property name becomes optional by that 
  * we mean that if you dont pass either color or width to the function 'createSquare' it wll not throw error
  * and the output will be '{color: "white", area: 100}'
  * 
